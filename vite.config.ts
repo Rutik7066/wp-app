10b8722128e3146564a6d29d3e79373266e8d201
Rutik7066 <rutikthakre@gmail.com>
2023-10-25 16:15:44 +0530

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,37 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
+  build: {
     target: 'esnext'
   },
-  plugins: [ react(),  VitePWA({ registerType: 'prompt' })],
+  plugins: [
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'WP',
+        short_name: 'WP',
+        theme_color: '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
