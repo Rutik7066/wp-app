7578920d0922b60e88d8cf02774dfc12c47f798f
Rutik7066 <rutikthakre@gmail.com>
2023-10-29 15:02:01 +0530

    fgh

--- src/routes/Dashboard/Home/Home.tsx
+++ src/routes/Dashboard/Home/Home.tsx
@@ -17,29 +17,31 @@ const Home = () => {
   return (
     <>
       <Stat />
-      <div className="flex flex-col w-full p-2 ">
-        <div className="flex justify-between  items-center">
-          <h1 className="text-white/80 font-bold text-lg mx-2">चालू जाहिरात</h1>
-          <NavLink to='/allcampaign' className='text-white bg-active rounded-lg  text-sm p-2 mx-2'>सर्व जाहिरात पहा</NavLink>
+      {data.expand.campaign != undefined ??
+        <div className="flex flex-col w-full p-2 ">
+          <div className="flex justify-between  items-center">
+            <h1 className="text-white/80 font-bold text-lg mx-2">चालू जाहिरात</h1>
+            <NavLink to='/allcampaign' className='text-white bg-active rounded-lg  text-sm p-2 mx-2'>सर्व जाहिरात पहा</NavLink>
+          </div>
+          <ul className="w-full my-2">
+            {data.expand.campaign.map((e, i) => e.status === "Running" && (
+              <NavLink key={i} to={`/allcampaign/${e.id}`}>
+                <li className="my-1 p-4 flex justify-between items-center text-white border-b border-secondary hover:bg-active/80 ">
+                  <h1 className="text-white font-bold ">{e.title}</h1>
+                  <div className="flex flex-row w-32  items-baseline  p-1 rounded-lg justify-between text-base  font-semibold text-gray-900 dark:text-white">
+                    <span className="relative flex h-3 w-3 mr-2 ml-2">
+                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                      <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                    </span>
+                    {e.status}
+                  </div>
+                </li>
+              </NavLink>
+            ))
+            }
+          </ul>
         </div>
-        <ul className="w-full my-2">
-          {data.expand.campaign.map((e, i) => e.status === "Running" && (
-            <NavLink key={i} to={`/allcampaign/${e.id}`}>
-              <li className="my-1 p-4 flex justify-between items-center text-white border-b border-secondary hover:bg-active/80 ">
-                <h1 className="text-white font-bold ">{e.title}</h1>
-                <div className="flex flex-row w-32  items-baseline  p-1 rounded-lg justify-between text-base  font-semibold text-gray-900 dark:text-white">
-                  <span className="relative flex h-3 w-3 mr-2 ml-2">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-                  </span>
-                  {e.status}
-                </div>
-              </li>
-            </NavLink>
-          ))
-          }
-        </ul>
-      </div>
+      }
       <Suspense key={"DaySpecial"} fallback={<Skeleton />}>
         <TodayEventGrid />
       </Suspense>
